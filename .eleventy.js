2aec827644804768c209a3aad77b943425a49ea5
francescoimola <frn.imola@gmail.com>
2025-03-13 20:35:51 +0000

    the start of something new

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,8 @@ const eleventySass = require("@11tyrocks/eleventy-plugin-sass-lightningcss");
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(eleventySass);
+  eleventyConfig.addPassthroughCopy("fonts");
+  eleventyConfig.addPassthroughCopy("assets");
 
   return {
     dir: {
